21854ff20858bf330738aa486833670dda4cd4bc
John Flottmeyer <6817440+johnflottmeyer@users.noreply.github.com>
2026-07-09 15:08:03 -0500

    Enhance characterState with additional attributes
    Added new properties for mouth shape, eye scale, lid rotation, lash opacity, and head transformations.

--- js/characterState.js
+++ js/characterState.js
@@ -3,6 +3,7 @@ window.characterState = {
         mouth: .2,
         mouthWidth: 28,
         mouthY: 25,
+        mouthShape: "smile",
 
         leftPupilX: 0,
         leftPupilY: 0,
@@ -11,13 +12,29 @@ window.characterState = {
 
         leftBlink: 0,
         rightBlink: 0,
+
+        leftEyeScaleY: 1,
+        rightEyeScaleY: 1,
+
+        leftLidRotate: 0,
+        rightLidRotate: 0,
+
         leftLash: 0,
         rightLash: 0,
+        lashOpacity: 1,
 
         leftUpper: 0,
         leftLower: 0,
         rightUpper: 0,
-        rightLower: 0
+        rightLower: 0,
+
+	headTurn: 0,
+	headSquash: 1,
+	depthOffset: 0,
+	featureOffset: 0,
+
+	leftArmShift: 0,
+	rightArmShift: 0,
     },
 
     idle: {},
